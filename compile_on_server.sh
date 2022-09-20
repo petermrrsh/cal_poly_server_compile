b669cb9b3cda0079d5fb48f592b7b4be7b81e70b
Peter Marsh <peter_marsh@hotmail.com>
2022-09-19 17:31:05 -0700

    compiles single c file on the server

---
+++ compile_on_server.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+# Edit these for personal use
+server="unix2.csc.calpoly.edu"
+user="pkmarsh"
+password="**************"
+
+print_usage() {
+    echo '*'
+    echo '* USAGE'
+    echo '* ./compile_on_server.sh <c file>'
+    echo '* '
+}
+
+if (( $# != 2 ))
+then
+    print_usage
+    exit 1
+fi
+
+c_file=$1
+
+server_cmd() {
+    ./server_login.expect -cmd $server $user $password $*
+}
+
+server_scp_to() {
+    ./server_login.expect -to $server $user $password $1 $2
+}
+
+server_scp_from() {
+    ./server_login.expect -from $server $user $password $1 $2
+}
+
+server_scp_to $c_file .
+server_cmd gcc $c_file
+server_cmd ./a.out
+server_scp_from output .
+server_scp_from error .
